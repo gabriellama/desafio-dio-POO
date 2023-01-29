a46cc6edf7ec6a0603902afe7c39d357bd3353ac
gabriellama <gabriellamalvares@gmail.com>
2023-01-29 00:39:01 -0300

    modificação final

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,41 +1,27 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
+
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
+    @Override
+    public double calcularXp() { return XP_PADRAO * cargaHoraria; }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public Curso() {
 
     }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setdescricao(String descricao) {
-        this.descricao = descricao;
-        
-    }
-    
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
+ 
+    public int getCargaHoraria() { return cargaHoraria;}
 
     public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-        
+        this.cargaHoraria = cargaHoraria;  
     }
+    
     @Override
     public String toString() {
         return "Mentoria{" +
-        "titulo='" + titulo + '\'' +
-        ", descricao'" + descricao + '\'' +
+        "titulo='" + getTitulo() + '\'' +
+        ", descricao'" + getDescricao() + '\'' +
         ", cargaHoraria=" + cargaHoraria +
         '}';
     
